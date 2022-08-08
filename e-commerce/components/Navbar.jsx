6c26080ad55baf26e42c0c7258da43524af30b71
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-08-08 22:21:39 +0530

    Functionalities added

--- e-commerce/components/Navbar.jsx
+++ e-commerce/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Link from 'next/link'
 import { AiOutlineShopping } from "react-icons/ai";
 
+import { Cart } from "./";
+
+import { Context } from "../context/StateContext";
+
 const Navbar = () => {
+
+  const { showCart, setShowCart, totalQuantities } = useContext(Context)
+
   return (
     <div className='navbar-container'>
       <p className='logo'>
@@ -12,10 +19,12 @@ const Navbar = () => {
         </Link>
       </p>
 
-      <button type='button' className='cart-icon' onClick="">
+      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   )
 }
